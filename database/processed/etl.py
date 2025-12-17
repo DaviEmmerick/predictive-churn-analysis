7b0f195204e80ca18e45418fbd101cca74336a27
DaviEmmerick <163426242+DaviEmmerick@users.noreply.github.com>
2025-12-16 23:07:40 -0300

    fix: new churn number

--- database/processed/etl.py
+++ database/processed/etl.py
@@ -1,28 +1,36 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 from datetime import datetime as dt
 
 df = pd.read_csv('database/raw/netflix_users.csv')
-#print(df.head())
-print('ETL process started...')
 
-def process_data(df):
-    df = df.drop(columns=['User_ID', 'Name']) # Dropping unnecessary columns to churn analysis
+def process_data(df, churn_threshold=180):
+    df = df.copy()
+    
+    df = df.drop(columns=['User_ID', 'Name'])
     df['Last_Login']  = pd.to_datetime(df['Last_Login'])
 
     max_date = df['Last_Login'].max()
     df['Days_Since_Last_Login'] = (max_date - df['Last_Login']).dt.days
-    df['Churn'] = (df['Days_Since_Last_Login'] > 50).astype(int)
+    
+    df['Churn'] = (df['Days_Since_Last_Login'] > churn_threshold).astype(int)
+    
     df = df.drop(columns=['Last_Login', 'Days_Since_Last_Login'])
 
     subs_map = {'Basic': 0, 'Standard': 1, 'Premium': 2}
     df['Subscription_Type'] = df['Subscription_Type'].map(subs_map)
-   
-    df = pd.get_dummies(df, columns=['Country', 'Favorite_Genre'], dtype=int)
-    #print(df.dtypes)
     
-    df.to_csv('database/processed/netflix_users_processed.csv', index=False)
-    print(f"Arquivo salvo com sucesso!")
+    df = pd.get_dummies(df, columns=['Country', 'Favorite_Genre'], dtype=int)
+    
+    filename = f'database/processed/netflix_users_processed_t{churn_threshold}.csv'
+    df.to_csv(filename, index=False)
+    
+    print(f"Arquivo salvo: {filename}")
+    print(f"Proporção Final usando {churn_threshold} dias:")
+    print(df['Churn'].value_counts(normalize=True))
+    
     return df
 
-df = process_data(df)
-print(df.head())
+df_final = process_data(df, churn_threshold=90) 
+print(df_final.head())
